8927574913b810994c79027ab4ba38eb6e6f71fd
Jitendar  Reddy <nadipireddy8@gmail.com>
2026-03-26 21:45:50 +0530

    Upadated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const auth = require("./auth");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -15,21 +14,19 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       role: "user"
     });
 
     res.status(201).json({ message: "User registered successfully" });
 
   } catch (error) {
-    console.error("REGISTER ERROR:", error);
+    console.error("REGISTER ERROR:",error);
     res.status(500).json({ message: error.message });
   }
 });
@@ -44,36 +41,22 @@ router.post("/login", async (req, res) => {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
-        // Compare password
         const isMatch = await bcrypt.compare(password, user.password);
-        
         if (!isMatch) {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        const token = jwt.sign({id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-        res.json({
-            token,
-            user: {
-                _id: user._id,
-                email: user.email
-            }
-        });
+        res.json({ token,
+          user:{
+            _id:user._id,
+            email:user.email
+          }
+         });
 
     } catch (err) {
-        console.error("Login error:", err);
         res.status(500).json({ message: "Server error" });
-    }
-});
-
-// GET all users (ADMIN)
-router.get("/", auth, async (req, res) => {
-    try {
-        const users = await User.find().select('-password');
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
     }
 });
 
