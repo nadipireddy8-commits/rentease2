8d03b8b16a5a6ef01b78a17d28fac71dd546f0bc
Jitendar  Reddy <nadipireddy8@gmail.com>
2026-03-27 11:32:09 +0530

    Upadated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
+const auth=require("./auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -21,5 +22,27 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+// UPDATE product
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
+// DELETE product
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 module.exports = router;
